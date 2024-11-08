cc59d1861e7310def3fc84bf88509b68a7522ff7
Jay Oh <jungwon.oh0920@gmail.com>
2024-11-07 23:25:11 -0800

    adding slider

--- src/components/YubuBanner.js
+++ src/components/YubuBanner.js
@@ -1,21 +1,49 @@
 import React from "react";
 import "./styles/banner.css";
-import Image from "react-bootstrap/Image";
-import Row from "react-bootstrap/Row";
+import Carousel from 'react-bootstrap/Carousel';
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 const YubuBanner = () => {
   return (
     <>
-      <h1 className="banner-heading z-1 position-absolute top-50 start-50">
-        Coming Soon 😜
-      </h1>
-      <Row className="position-absolute top-0 start-0 end-0">
-        <Image
-          src={require("../assets/banner.JPEG")}
-          className="banner-image vh-100"
-          rounded
-        />
-      </Row>
+      <Carousel>
+        <Carousel.Item interval={1500}>
+          <div className="h-128 relative">
+            <img
+              className="absolute left-1/2 h-48 -translate-x-1/2 translate-y-1/2 z-1"
+              src={require("../assets/singleYubu.png")}
+              alt="solo-logo"
+            />
+            <img
+              className="w-full h-full object-cover opacity-50"
+              src={require("../assets/banner.JPEG")}
+              alt="first-banner"
+            />
+          </div>
+          <Carousel.Caption>
+            <h3 className="text-amber-400 text-5xl" style={{textShadow: "2px 2px #000000"}}>Seattle Yubu Coming Soon</h3>
+            <p className="text-amber-400 text-3xl" style={{textShadow: "2px 2px #000000"}}>Fun-filled tofu pockets topped with your favorite flavors.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <div className="h-128">
+            <img
+              className="w-full h-full object-cover"
+              src={require("../assets/yubuTasting.jpg")}
+              alt="second"
+            />
+          </div>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <div className="h-128">
+            <img
+              className="w-full h-full object-cover"
+              src={require("../assets/test.png")}
+              alt="second"
+            />
+          </div>
+        </Carousel.Item>
+      </Carousel>
     </>
   );
 };
