58fd4d741224eb9accf6bf0a921ce2463b15dbc7
Jay Oh <jungwon.oh0920@gmail.com>
2024-11-08 17:30:41 -0800

    working on insta comp.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import YubuNavBar from "./components/YubuNavBar";
 import YubuBanner from "./components/YubuBanner";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Insta from "./components/Insta";
 
 function App() {
   return (
     <div className="App">
       <YubuNavBar />
       <YubuBanner />
+      <Insta />
     </div>
   );
 }
--- src/components/Insta.js
+++ src/components/Insta.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {InstagramEmbed} from 'react-social-media-embed';
 
 function Insta() {
     return (
-        <div>Insta</div>
+        <section className='my-12'>
+            <div className='max-w-5xl block mx-auto my-12 text-center border-b-4 border-amber-400'>
+                <h2>New Updates</h2>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <InstagramEmbed url="https://www.instagram.com/p/DCDx2gUxgun/" width={328} />
+            </div>
+        </section>
     )
 }
 
